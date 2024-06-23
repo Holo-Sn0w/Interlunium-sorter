c14f29bd1c3a0f63e9b8c8f6e2dbe96f1b05debd
Holo-Sn0w <koguihope2004@gmail.com>
2024-06-23 22:51:54 +0200

    Update 2024-06-22.js

--- src/js/data/2024-06-22.js
+++ src/js/data/2024-06-22.js
@@ -19,140 +19,140 @@ dataSet[dataSetVersion].options = [
 dataSet[dataSetVersion].characterData = [
   {
     name: "Rinko Hanazawa",
-    img: "XSaoKHc.jpg",
+    img: "https://i.imgur.com/XSaoKHc.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Junko",
-    img: "SQoVbHD.jpg",
+    img: "https://i.imgur.com/SQoVbHD.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Dan Bi Han",
-    img: "eLgcApv.jpg",
+    img: "https://i.imgur.com/eLgcApv.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Megumi Aihara",
-    img: "O0ycTzm.jpg",
+    img: "https://i.imgur.com/O0ycTzm.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Chiyo Watanabe",
-    img: "0SCqXjG.jpg",
+    img: "https://i.imgur.com/0SCqXjG.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Kanon Mistuboshi",
-    img: "BNGcAJk.jpg",
+    img: "https://i.imgur.com/BNGcAJk.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Eimi Inagawa",
-    img: "iyi7Qeb.jpg",
+    img: "https://i.imgur.com/iyi7Qeb.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Akira Hoshino",
-    img: "HF6er2E.jpg",
+    img: "https://i.imgur.com/HF6er2E.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Yume Zhou",
-    img: "m5FaCBx.jpg",
+    img: "https://i.imgur.com/m5FaCBx.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Anette Chanel",
-    img: "qLDIDKi.jpg",
+    img: "https://i.imgur.com/qLDIDKi.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Kazue Ono",
-    img: "6EAjcL7.jpg",
+    img: "https://i.imgur.com/6EAjcL7.jpg",
     opts: {
       Groups: ["IL"]
     }
   },
   {
     name: "Natsuki Ichinose",
-    img: "wZSIQ1z.jpg",
+    img: "https://i.imgur.com/wZSIQ1z.jpg",
     opts: {
       Groups: ["KB"]
     }
   },
   {
     name: "Hayate Sumiyoshi",
-    img: "gs4baav.jpg",
+    img: "https://i.imgur.com/gs4baav.jpg",
     opts: {
       Groups: ["KB"]
     }
   },
   {
     name: "Yuu Tobe",
-    img: "DDCvPt6.jpg",
+    img: "https://i.imgur.com/DDCvPt6.jpg",
     opts: {
       Groups: ["KB"]
     }
   },
   {
     name: "Erika Horie",
-    img: "62IgzSg.jpg",
+    img: "https://i.imgur.com/62IgzSg.jpg",
     opts: {
       Groups: ["PP"]
     }
   },
   {
     name: "Mariya Akiyama",
-    img: "AtrUM4d.jpg",
+    img: "https://i.imgur.com/AtrUM4d.jpg",
     opts: {
       Groups: ["PP"]
     }
   },
   {
     name: "Kohaku Peterson",
-    img: "1yaBqUM.jpg",
+    img: "https://i.imgur.com/1yaBqUM.jpg",
     opts: {
       Groups: ["PP"]
     }
   },
   {
     name: "Tae-eun Song",
-    img: "vejwF22.jpg",
+    img: "https://i.imgur.com/vejwF22.jpg",
     opts: {
       Groups: ["PP"]
     }
   },
   {
     name: "Ai Nakajima",
-    img: "SBjPq66.jpg",
+    img: "https://i.imgur.com/SBjPq66.jpg",
     opts: {
       Groups: ["PP"]
     }
   },
   {
     name: "Ume Takeuchi",
-    img: "ZERokj1.jpg",
+    img: "https://i.imgur.com/ZERokj1.jpg",
     opts: {
       Groups: ["Ume"]
     }
